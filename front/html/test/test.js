f42d1d6af6f57f6ba4ef2707561fbbefc78abf74
Halcisse <83637718+Halcisse@users.noreply.github.com>
2022-03-02 15:28:59 +0100

    INSERTION PRODUITS ACCUEIL OK

---
+++ front/html/test/test.js
@@ -0,0 +1,37 @@
+/* J'initialise la variable article qui contiendra mon api */
+let articles = [];
+
+/* Je crée une fonction qui récupére tout les articles depuis mon API */
+async function fetchApi() {
+  await fetch("https://api-kanap-eu.herokuapp.com/api/products")
+    .then((res) => res.json())
+    // Je stock le tout dans une variable articles
+    .then((data) => (articles = data));
+}
+console.log(articles);
+/* Je crée une fonction avec une boucle pour afficher mes vignettes */
+async function canapDisplay() {
+  await fetchApi();
+  let items = document.getElementById("items");
+
+  for (let i = 0; i < articles.length; i++) {
+    let link = document.createElement("a");
+    let cart__item = document.createElement("article");
+    let cart__item_img = document.createElement("img");
+    let cart__item_h3 = document.createElement("h3");
+    let cart__item_p = document.createElement("p");
+
+    items.appendChild(link);
+    link.appendChild(cart__item);
+    cart__item.append(cart__item_img, cart__item_h3, cart__item_p);
+
+    link.href = `./product.html?id=${articles[i]._id}`;
+    cart__item_img.alt = articles[i].altTxt;
+    cart__item_img.src = articles[i].imageUrl;
+    cart__item_h3.classList.add("productName");
+    cart__item_h3.textContent = articles[i].name;
+    cart__item_p.classList.add("productDescription");
+    cart__item_p.textContent = articles[i].description;
+  }
+}
+canapDisplay();
